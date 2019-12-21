a1e1d9d8b3b14a403b8dbc0b33fd471649411f22
James <jmiracles80@gmail.com>
2019-12-21 16:52:46 -0600

    Update diagonalDiff.py

--- diagonalDiff.py
+++ diagonalDiff.py
@@ -4,7 +4,7 @@
 
 class Solution:
 
-  def diagonalDifference(arr):
+  def diagonalDifference(self,arr):
 
     total_1, total_2 = 0,0
     revIn = len(arr) -1
@@ -15,3 +15,12 @@ class Solution:
         total_2 += arr[i][revIn - i]
 
     return abs( total_1 - total_2)
+
+  
+  
+if __name__ == '__main__':
+  
+    arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+    
+    
+    print(diagonalDifference(arr))
